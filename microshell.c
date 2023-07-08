ad949bddcbc635721723bee9c0d38b7eb9271c8b
miri03 <haritmeryem@gmail.com>
2023-07-08 15:42:27 +0100

    success

--- microshell.c
+++ microshell.c
@@ -6,7 +6,7 @@
 /*   By: meharit <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 18:53:35 by meharit           #+#    #+#             */
-/*   Updated: 2023/07/07 19:11:02 by meharit          ###   ########.fr       */
+/*   Updated: 2023/07/08 15:40:41 by meharit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,40 +14,38 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-void	error_mess(char *msg, int status)
+#define TRUE 1
+#define FALSE 0
+
+int	error_mess(char *msg, int status, int st_exit)
 {
-	while (*msg)
+	while (msg && *msg)
 	{
 		write(2, msg, 1);
 		msg++;
 	}
-	if (status > 0)
+	if (st_exit == TRUE)
 		exit(status);
+	return (1);
 }
 
-void	ft_cd(char **argv, int i, int cmd)
+int	ft_cd(char **argv, int i, int cmd)
 {
-	printf("----> i %d\n", i);
-	if (i >= 4)
-	{
-		error_mess("error: cd: bad arguments\n", -1);
-		return;
-	}
+	if (i - cmd != 2)
+		return (error_mess("error: cd: bad arguments\n", 1, FALSE));
 	if (chdir(argv[cmd+1]) == -1)
-	{
-		error_mess("error: cd: cannot change directory to ", -1);
-		error_mess(argv[cmd+1], -1);
-		error_mess("\n", -1);
-	}
+		return (error_mess("error: cd: cannot change directory to ", 1, FALSE), error_mess(argv[cmd+1], 1, FALSE), error_mess("\n", 1, FALSE));
+	return (0);
 }
 
-int	pipe_next(char **argv, int i)
+int	pipe_next(char **argv, int i, int cmd)
 {
-//	printf("i -->%d %s\n", i, argv[i]);
 	if (argv[i] && !strcmp(argv[i], "|"))
 		return (1);
-//	printf("9alwa\n");
+	if (argv[cmd] && !strcmp(argv[cmd], ";"))
+		return (-1);
 	return (0);
 }
 
@@ -58,29 +56,23 @@ int	exec(char **argv, char **path, int i, int cmd)
 	int	pid;
 	int	status;
 
-	w_pipe = pipe_next(argv, i);
-//	printf("pp-> %d\n", w_pipe);
+	w_pipe = pipe_next(argv, i, cmd);
+	if (w_pipe == -1)
+		return (0);
 	if (w_pipe && pipe(pp) == -1)
-		error_mess("error: fatal\n", 1);
+		return (error_mess("error: fatal\n", 1, TRUE));
 	pid = fork();
 	if (pid == 0)
 	{
-//		printf("before = %s   ", argv[i]);
 		argv[i] = NULL;
-//		printf("after = %s\n", argv[i]);
 		if (w_pipe && (dup2(pp[1], 1) == -1 || close(pp[0]) == -1 || close(pp[1]) == -1))
-			error_mess("error: fatal\n", 1);
-//		dprintf(2, "cmd = %d argv[cmd] = %s\n", cmd, argv[cmd]);
+			return(error_mess("error: fatal\n", 1, TRUE));
 		if (execve(argv[cmd], &argv[cmd], path) == -1)
-		{
-			error_mess("error: cannot execute ", -1);
-			error_mess(argv[cmd], -1);
-			error_mess("\n", 127);
-		}
+			return (error_mess("error: cannot execute ", 127, FALSE), error_mess(argv[cmd], 127, FALSE), error_mess("\n", 127, FALSE));
 	}
 	waitpid(pid, &status, 0);
 	if (w_pipe && (dup2(pp[0], 0) == -1 || close(pp[0]) == -1 || close(pp[1]) == -1))
-		error_mess("error: fatal\n", 1);
+		error_mess("error: fatal\n", 1, TRUE);
 	if (WIFEXITED(status))
 		status = WEXITSTATUS(status);
 	return (status);
@@ -94,6 +86,7 @@ int main(int argc, char **argv, char **path)
 	int	cmd;
 
 	i = 1;
+	status = 0;
 	if (argc > 1)
 	{
 		while (argv[i])
@@ -103,13 +96,13 @@ int main(int argc, char **argv, char **path)
 			while (argv[i] && (strcmp(argv[i], "|") && strcmp(argv[i], ";")))
 				i++; //index of "|" or ";" or NULL
 			if (!(strcmp(argv[cmd], "cd")))
-				ft_cd(argv, i,cmd);
+				status = ft_cd(argv, i,cmd);
 			else
 				status = exec(argv, path, i, cmd);
 			if (argv[i])
 				i++;
 		}
 	}
-	return (0);
+	return (status);
 }
 
